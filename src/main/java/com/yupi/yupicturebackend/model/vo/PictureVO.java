2e21f69147240837eb92dc7160fc4991fee6394b
askyikai <askyikai@qq.com>
2025-03-24 20:22:27 +0800

    data model

---
+++ src/main/java/com/yupi/yupicturebackend/model/vo/PictureVO.java
@@ -0,0 +1,114 @@
+package com.yupi.yupicturebackend.model.vo;
+
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.annotation.*;
+import com.yupi.yupicturebackend.model.entity.Picture;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 图片VO
+ */
+@Data
+public class PictureVO implements Serializable {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 图片 url
+     */
+    private String url;
+
+    /**
+     * 图片名称
+     */
+    private String name;
+
+    /**
+     * 简介
+     */
+    private String introduction;
+
+    /**
+     * 分类
+     */
+    private String category;
+
+    /**
+     * 标签（JSON 数组）to List
+     */
+    private List<String> tags;
+
+    /**
+     * 图片体积
+     */
+    private Long picSize;
+
+    /**
+     * 图片宽度
+     */
+    private Integer picWidth;
+
+    /**
+     * 图片高度
+     */
+    private Integer picHeight;
+
+    /**
+     * 图片宽高比例
+     */
+    private Double picScale;
+
+    /**
+     * 图片格式
+     */
+    private String picFormat;
+
+    /**
+     * 创建用户 id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 编辑时间
+     */
+    private Date editTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public static Picture voToPicture(PictureVO pictureVO) {
+        if (pictureVO == null) {
+            return null;
+        }
+        Picture picture = new Picture();
+        BeanUtils.copyProperties(pictureVO, picture);
+        picture.setTags(JSONUtil.toJsonStr(pictureVO.getTags()));
+        return picture;
+    }
+
+    public static PictureVO pictureToVO(Picture picture) {
+        if (picture == null) {
+            return null;
+        }
+        PictureVO pictureVO = new PictureVO();
+        BeanUtils.copyProperties(picture, pictureVO);
+        pictureVO.setTags(JSONUtil.toList(picture.getTags(), String.class));
+        return pictureVO;
+    }
+}
---
+++ src/main/java/com/yupi/yupicturebackend/service/PictureService.java
@@ -0,0 +1,13 @@
+package com.yupi.yupicturebackend.service;
+
+import com.yupi.yupicturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author ken
+* @description 针对表【picture(图片)】的数据库操作Service
+* @createDate 2025-03-24 19:54:21
+*/
+public interface PictureService extends IService<Picture> {
+
+}
